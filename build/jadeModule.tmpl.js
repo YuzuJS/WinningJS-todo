81c0f0b0cf8c95f6f57917efa97437ab5bc2b3ba
jfkhoury <jkhoury@book.com>
2012-06-01 16:31:22 -0400

    Fix jadeModule single element issue
    Populate container before checking for children

--- build/jadeModule.tmpl.js
+++ build/jadeModule.tmpl.js
@@ -3,7 +3,8 @@ module.exports = <%= templateFunctionSource %>;
 module.exports.toElement = function (locals) {
     var html = module.exports(locals);
     var containerEl = document.createElement("div");
-    if (containerEl.childElementCount !== 1) {
+    containerEl.innerHTML = html;
+    if (containerEl.children.length !== 1) {
         throw new Error("toElement can only be called on Jade templates that contain a single element");
     }
     return containerEl.children[0];
