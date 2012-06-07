2d4ca10c4f672dac9f021b697c4c72bbff321ed9
Domenic Denicola <domenic@domenicdenicola.com>
2012-06-07 17:45:32 -0400

    Output build errors in Visual Studio format to the console.
    Fixes #7.

--- build/buildIndexTask.js
+++ build/buildIndexTask.js
@@ -6,6 +6,7 @@ var path = require("path");
 var jade = require("jade");
 var doBrowserify = require("./doBrowserify");
 var doStylus = require("./doStylus");
+var getStackFrames = require("./utils").getStackFrames;
 
 var baseDir = path.resolve(__dirname, ".."); // TODO don't do this, only browserify needs it, and maybe it doesn't.
 
@@ -18,7 +19,7 @@ module.exports = function (grunt) {
         grunt.log.writeln("File \"" + dest + "\" created");
     }
 
-    grunt.registerTask("buildIndex", "Browserify modules, compile Stylus, and build an index.html.", function () {
+    function doBuildIndex() {
         var browserifyConfig = grunt.config("buildIndex.browserify");
         var scripts = doBrowserify(grunt, baseDir, browserifyConfig);
 
@@ -34,5 +35,22 @@ module.exports = function (grunt) {
             styles: styles,
             entryModule: "./" + browserifyConfig.entry
         });
+    }
+
+    grunt.registerTask("buildIndex", "Browserify modules, compile Stylus, and build an index.html.", function () {
+        try {
+            doBuildIndex();
+        } catch (error) {
+            var frames = getStackFrames(error);
+
+            var fileName = frames[0].getFileName();
+            var line = frames[0].getLineNumber();
+            var column = frames[0].getColumnNumber();
+            var code = error.name;
+            var message = error.message;
+
+            // Visual Studio error format: http://msdn.microsoft.com/en-us/library/yxkt8b26%28v=vs.110%29.aspx
+            console.error(fileName + "(" + line + "," + column + "): error " + code + ": " + message);
+        }
     });
 };
--- build/utils.js
+++ build/utils.js
@@ -3,3 +3,23 @@
 exports.urlize = function (path) {
     return "/" + path.replace(/\\/g, "/");
 };
+
+exports.getStackFrames = function (error) {
+    // http://code.google.com/p/v8/wiki/JavaScriptStackTraceApi
+
+    // Replace the `Error.prepareStackTrace` function temporarily, to capture the frames.
+    var oldErrorPrepareStackTrace = Error.prepareStackTrace;
+    var frames = null;
+    Error.prepareStackTrace = function (theError, theFrames) {
+        frames = theFrames;
+    };
+
+    /*jshint expr: true */
+    // Calling the `error.stack` getter triggers our custom `Error.prepareStackTrace` function above.
+    error.stack;
+
+    // Clean up after ourselves.
+    Error.prepareStackTrace = oldErrorPrepareStackTrace;
+
+    return frames;
+};
