a2be517d343cb7aec946281df076063f704a4850
pbouzakis <github@paulbouzakis.com>
2012-06-15 17:22:36 -0400

    Add ko bindings on app.launch.  Resolves Issue #28.
    Update TodoList component template to use custom ko binding `itemInvoked`

--- components/TodosPage/TodoList/index.js
+++ components/TodosPage/TodoList/index.js
@@ -9,23 +9,24 @@ var itemTemplate = require("./itemTemplate.jade");
 module.exports = function TodosPageTodoList(todos, showCommands) {
     var that = this;
 
+    // Keeping view model logic inside our component for now, because it is really just one function
+    // Might move out, if presenter or view logic requires more logic.
+    function selectItems(winControl) {
+        // Note a few wierd things: `getItems()` returns a promise, and the array it returns is of `{ key, data }`
+        // objects, not just the elements of the collection. Think about how we want to deal with this.
+        console.log("[TodosPageTodoList] selectItems");
+        var items = Q.when(winControl.selection.getItems());
+        showCommands("todos-selected", items);
+    }
+
     var presenter = new Presenter({
         template: template,
         ui: {
             itemDataSource: todos.dataSource,
             itemTemplate: new WinJS.Binding.Template(itemTemplate.toElement())
-        }
-    });
-
-    presenter.winControl.then(function (winControl) {
-        winControl.addEventListener("iteminvoked", function () {
-            // Note a few wierd things: `getItems()` returns a promise, and the array it returns is of `{ key, data }`
-            // objects, not just the elements of the collection. Think about how we want to deal with this.
-            var items = Q.when(winControl.selection.getItems());
-
-            showCommands("todos-selected", items);
-        });
-    });
+        },
+        viewModel: { selectItems: selectItems }
+    });               
 
     that.render = presenter.process;
 };
